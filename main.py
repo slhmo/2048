b3c6f4111e3328f0b6fde60066c164717ef049a3
slhmo <mohamadzadehsaleh7@gmail.com>
2024-12-11 17:19:56 +0330

    uppward move done

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import random
 
 game_plain = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-# game_plain[3][3] = 5
+flag_plain = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+# this plain is used to see if a value has been changed or not
+# (it will be restored after every move)
 
 
 def block_generator(plain):
@@ -14,8 +16,9 @@ def block_generator(plain):
         return plain
 
 
-def move_maker(plain):
+def move_maker(plain, flag):
     move = input(': ')
+    not_changed = True
     if move == 'w':
         i = 1
         while i<4:
@@ -26,30 +29,39 @@ def move_maker(plain):
 
                     while b>0:
                         if plain[b-1][j] == 0:
+                            not_changed = False
                             tmp = plain[b][j]
                             plain[b][j] = 0
                             plain[b-1][j] = tmp
-                        elif plain[b-1][j] == plain[b][j] != 0:
+                        elif (plain[b-1][j] == plain[b][j] != 0 and
+                              flag[b-1][j] == flag[b][j] == 0):
+                            not_changed = False
                             plain[b-1][j] *= 2
+                            flag[b-1][j] = 1
                             plain[b][j] = 0
                         b -= 1
                 j += 1
             i += 1
 
 
+    if not_changed :
+        move_maker(plain, flag)
+
+
 block_generator(game_plain)
 block_generator(game_plain)
 
 
-def play(plain):
+def play(plain, flag):
+
+    flag = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
     for i1 in range(4):
         print(game_plain[i1])
 
-    move_maker(plain)
+    move_maker(plain, flag)
     block_generator(plain)
+    play(plain, flag)
 
-    play(plain)
 
-
-play(game_plain)
+play(game_plain, flag_plain)
