308bc140ca9973eb246a85a3b745f4e0391a47f8
slhmo <mohamadzadehsaleh7@gmail.com>
2024-12-11 22:14:26 +0330

    base done

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def block_generator(plain):
 def move_maker(plain, flag):
     move = input(': ')
     not_changed = True
+
     if move == 'w':
         i = 1
         while i<4:
@@ -42,6 +43,81 @@ def move_maker(plain, flag):
                         b -= 1
                 j += 1
             i += 1
+
+
+    if move == 's':
+        i = 2
+        while i>-1:
+            j = 3
+            while j>-1:
+                if plain[i][j] != 0:
+                    b = i
+
+                    while b<3:
+                        if plain[b+1][j] == 0:
+                            not_changed = False
+                            tmp = plain[b][j]
+                            plain[b][j] = 0
+                            plain[b+1][j] = tmp
+                        elif (plain[b+1][j] == plain[b][j] != 0 and
+                              flag[b+1][j] == flag[b][j] == 0):
+                            not_changed = False
+                            plain[b+1][j] *= 2
+                            flag[b+1][j] = 1
+                            plain[b][j] = 0
+                        b += 1
+                j -= 1
+            i -= 1
+
+
+    if move == 'a':
+        j = 1
+        while j<4:
+            i = 0
+            while i<4:
+                if plain[i][j] != 0:
+                    b = j
+
+                    while b>0:
+                        if plain[i][b-1] == 0:
+                            not_changed = False
+                            tmp = plain[i][b]
+                            plain[i][b] = 0
+                            plain[i][b-1] = tmp
+                        elif (plain[i][b-1] == plain[i][b] != 0 and
+                              flag[i][b-1] == flag[i][b] == 0):
+                            not_changed = False
+                            plain[i][b-1] *= 2
+                            flag[i][b-1] = 1
+                            plain[i][b] = 0
+                        b -= 1
+                i += 1
+            j += 1
+
+
+    if move == 'd':
+        j = 2
+        while j>-1:
+            i = 3
+            while i>-1:
+                if plain[i][j] != 0:
+                    b = j
+
+                    while b<3:
+                        if plain[i][b+1] == 0:
+                            not_changed = False
+                            tmp = plain[i][b]
+                            plain[i][b] = 0
+                            plain[i][b+1] = tmp
+                        elif (plain[i][b+1] == plain[i][b] != 0 and
+                              flag[i][b+1] == flag[i][b] == 0):
+                            not_changed = False
+                            plain[i][b+1] *= 2
+                            flag[i][b+1] = 1
+                            plain[i][b] = 0
+                        b += 1
+                i -= 1
+            j -= 1
 
 
     if not_changed :
